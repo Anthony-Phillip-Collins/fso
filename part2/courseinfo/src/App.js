c4f127d1c937691900a07fbd4eda9db4b35d3720
Anthony-Phillip-Collins <anthony.phillip.collins@gmail.com>
2022-09-16 20:31:01 +1000

    2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,33 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Part = ({ name, exercises }) => (
-  <p>
-    {name} {exercises}
-  </p>
-);
-
-const Content = ({ parts }) =>
-  parts.map((part) => <Part key={part.id} {...part} />);
-
-const Total = ({ parts }) => {
-  return (
-    <strong>
-      total of{' '}
-      {parts && parts.length > 0
-        ? parts.reduce((total, { exercises }) => total + exercises, 0)
-        : 0}{' '}
-      exercises
-    </strong>
-  );
-};
-
-const Course = ({ course: { name, parts } }) => (
-  <>
-    <Header course={name} />
-    <Content parts={parts} />
-    <Total parts={parts} />
-  </>
-);
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
